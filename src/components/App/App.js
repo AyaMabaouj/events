df9137622279ba35084cf15e88f6885814e64d4a
aya mabaouj <mabaoujaya@gmail.com>
2022-12-18 12:15:24 +0100

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import FileNamer from '../FileNamer/FileNamer';
+function App() {
+  return (
+    <div className="App">
+      <FileNamer></FileNamer>
+    </div>
+  );
+}
+
+export default App;
